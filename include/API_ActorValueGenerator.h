f55019e576aef67870a2ed0e3436ff878d1b1bba
NoahBoddie <101380447+NoahBoddie@users.noreply.github.com>
2023-02-15 18:33:22 -0500

    Added API Header Files

---
+++ include/API_ActorValueGenerator.h
@@ -0,0 +1,237 @@
+#pragma once
+
+//#include <string_view>
+
+
+
+#define ARTH_OBJECT_TYPE RE::TESForm
+#define ARTH_CONTEXT_TYPE RE::ExtraDataList
+#define ARTH_ENUM_TYPE RE::FormType
+
+#define ARITHMETIC_API_SOURCE "ActorValueExtension.dll"
+#define ARITHMETIC_API_SOURCE_L L"ActorValueExtension.dll"
+
+#include <API_Arithmetic.h>
+
+
+
+namespace ActorValueGeneratorAPI
+{
+	//RE::Actor* target, RE::Actor* aggressor, std::string av_name, ActorValueModifer mod(total = base), float value
+	using ExportFunction = void(*)(RE::Actor*, RE::Actor*, std::string, RE::ACTOR_VALUE_MODIFIER, float);
+
+
+	enum Version
+	{
+		Version1,
+
+		Current = Version1
+	};
+
+	struct InterfaceVersion1
+	{
+		inline static constexpr auto VERSION = Version::Version1;
+
+		virtual ~InterfaceVersion1() = default;
+
+		/// <summary>
+		/// Gets the current version of the interface.
+		/// </summary>
+		/// <returns></returns>
+		virtual Version GetVersion() = 0;
+
+
+		//Target will have to be defined here if there's no source, just for the transfers sake. It doesn't nothing special anyhow.
+		
+		/// <summary>
+		/// Registers a function to able to be called upon with it's set functions. Currently must be called in the event PostLoad
+		/// </summary>
+		/// <param name="export_name"></param>
+		/// <param name="function"></param>
+		virtual void RegisterExportFunction(std::string export_name, ExportFunction function) = 0;
+
+		/// <summary>
+		/// Checks an actor value, turning it into the actor value the name designated.
+		/// </summary>
+		/// <param name="av_ref">is actor value index to alter. Will be set to total upon failure.</param>
+		/// <param name="av_name">is name of the ActorValue or ExtraValue requested.</param>
+		virtual void CheckActorValue(RE::ActorValue& av_ref, const char* av_name) = 0;
+
+		/// <summary>
+		/// Sets the AVDelay of an actor value. Currently unimplement.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="actor_value"></param>
+		/// <param name="new_delay"></param>
+		/// <returns></returns>
+		virtual bool SetAVDelay(RE::Actor* target, RE::ActorValue actor_value, float new_delay) = 0;
+
+	};
+
+	using CurrentInterface = InterfaceVersion1;
+
+	
+	inline CurrentInterface* Interface{ nullptr };
+
+#ifdef AVG_SOURCE 
+
+	CurrentInterface* InferfaceSingleton();
+
+	namespace detail
+	{
+		extern "C" __declspec(dllexport) void* AVG_RequestInterfaceImpl(Version version)
+		{
+			CurrentInterface* result = InferfaceSingleton();
+
+			switch (version)
+			{
+			case Version::Version1:
+				return dynamic_cast<InterfaceVersion1*>(result);
+
+			}
+
+			return nullptr;
+		}
+	}
+
+#endif
+
+	/// <summary>
+	/// Accesses the ActorValueGenerator Interface. Using the template version is advised. Safe to call PostLoad
+	/// </summary>
+	/// <param name="version"> to request.</param>
+	/// <returns>Returns void* of the interface, cast to the respective version.</returns>
+	inline void* RequestInterface(Version version)
+	{
+		typedef void* (__stdcall* RequestFunction)(Version);
+
+		static RequestFunction request_interface = nullptr;
+
+		HINSTANCE API = GetModuleHandle(ARITHMETIC_API_SOURCE_L);
+
+		if (API == nullptr) {
+			logger::critical("ActorValueExtension.dll not found, API will remain non functional.");
+			return nullptr;
+		}
+
+		request_interface = (RequestFunction)GetProcAddress(API, "AVG_RequestInterfaceImpl");
+
+
+		if (request_interface) {
+			if (static unsigned int once = 0; once++)
+			logger::info("Successful module and request, AVG");
+		}
+		else {
+			logger::critical("Unsuccessful module and request, AVG");
+			return nullptr;
+		}
+
+		auto intfc = (CurrentInterface*)request_interface(version);
+
+		return intfc;
+	}
+	
+	/// <summary>
+	/// Accesses the ActorValueGenerator Interface, safe to call PostLoad
+	/// </summary>
+	/// <typeparam name="InterfaceClass">is the class derived from the interface to use.</typeparam>
+	/// <returns>Casts to and returns a specific version of the interface.</returns>
+	template <class InterfaceClass = CurrentInterface>
+	inline  InterfaceClass* RequestInterface()
+	{
+		static InterfaceClass* intfc = nullptr;
+
+		if (!intfc) {
+			intfc = reinterpret_cast<InterfaceClass*>(RequestInterface(InterfaceClass::VERSION));
+
+			if constexpr (std::is_same_v<InterfaceClass, CurrentInterface>)
+				Interface = intfc;
+		}
+
+		return intfc;
+	}
+
+}
+#ifndef AVG_SOURCE
+
+namespace AVG
+{
+	
+	template<size_t N>
+	struct StringLiteral {
+		static constexpr size_t Size = N;
+		constexpr StringLiteral(const char(&str)[N]) {
+			std::copy_n(str, N, value);
+		}
+
+		constexpr operator const char* () { return &value; }
+
+
+		constexpr operator std::string_view() { return std::string_view(&value, Size); }
+
+
+		constexpr std::string_view view() { return std::string_view(&value, Size); }
+
+		char value[N];
+	};
+
+	/// <summary>
+	/// A class used to store and represent an actor value that may or may not be an Extra Value. Set with a string to get its EV.
+	/// </summary>
+	struct DynamicValueID
+	{
+		RE::ActorValue actorValue = RE::ActorValue::kNone;
+
+		DynamicValueID() = default;
+
+		constexpr DynamicValueID(RE::ActorValue a_rhs) : actorValue(a_rhs) {}
+
+		template <class StringType> requires (std::is_same_v<StringType, std::string_view> || std::is_same_v<StringType, std::string>)
+			DynamicValueID(StringType a_rhs)
+		{
+			if (ActorValueGeneratorAPI::RequestInterface())
+				ActorValueGeneratorAPI::Interface->CheckActorValue(actorValue, a_rhs);
+		}
+
+
+		constexpr DynamicValueID& operator=(RE::ActorValue a_rhs)
+		{
+			actorValue = a_rhs;
+			return *this;
+		}
+
+
+		template <class StringType> requires (std::is_same_v<StringType, std::string_view> || std::is_same_v<StringType, std::string>)
+			DynamicValueID& operator=(StringType a_rhs)
+		{
+			if (ActorValueGeneratorAPI::RequestInterface())
+				ActorValueGeneratorAPI::Interface->CheckActorValue(actorValue, a_rhs);
+			return *this;
+		}
+	};
+
+	/// <summary>
+	/// A constant Value ID that holds onto the ActorValue of it's associated string.
+	/// </summary>
+	/// <typeparam name="AV_Name">The string literal name of the AV. Case insensitive.</typeparam>
+	template <StringLiteral AV_Name>
+	struct ConstValueID
+	{
+		//
+
+		static constexpr auto ActorValueName = AV_Name.value;
+
+		operator RE::ActorValue() const
+		{
+			if (ActorValueGeneratorAPI::RequestInterface())
+				ActorValueGeneratorAPI::Interface->CheckActorValue(_actorValueID, ActorValueName);
+
+			return _actorValueID;
+		}
+
+	private:
+		static inline RE::ActorValue _actorValueID = RE::ActorValue::kNone;
+	};
+}
+
+#endif
---
+++ include/API_Arithmetic.h
@@ -0,0 +1,283 @@
+#pragma once
+
+#ifndef ARITHMETIC_API_SOURCE
+static_assert(false, "Macro 'ARITHMETIC_API_SOURCE' isn't defined.");
+#endif
+
+
+namespace Arthmetic
+{
+
+	struct Target;
+	struct ResolvedArg;
+	struct DelegateArgument;
+
+	//Currently unimplemented.
+	using ParamConstraints = uint64_t;
+
+#ifdef ARITHMETIC_SOURCE
+	using SWITCH_ARG = DelegateArgument;
+#else
+	using SWITCH_ARG = ResolvedArg;
+#endif
+
+
+	using ArgumentList = std::vector<SWITCH_ARG*>;
+
+
+#ifdef ARTH_OBJECT_TYPE
+	using BaseObject = ARTH_OBJECT_TYPE;
+#else 
+	using BaseObject = void;
+#endif
+
+
+#ifdef ARTH_CONTEXT_TYPE
+	using TargetContext = ARTH_CONTEXT_TYPE;
+#else 
+	using TargetContext = void;
+#endif
+
+
+#ifdef ARTH_ENUM_TYPE
+	using TargetType = ARTH_ENUM_TYPE;
+#else 
+	using TargetType = std::uint32_t;
+#endif
+
+}
+
+
+namespace ArithmeticAPI
+{
+
+	using NativeFormula = float(*)(Arthmetic::Target, const Arthmetic::ArgumentList&);
+
+
+
+	using LinkNatFunc = int(*)(std::string_view, int, NativeFormula);
+
+
+
+	using SWITCH_ARG = Arthmetic::SWITCH_ARG;
+
+	enum Version
+	{
+		Version1,
+
+
+		Current = Version1
+	};
+
+	struct InterfaceVersion1
+	{
+		inline static constexpr auto VERSION = Version::Version1;
+
+		virtual ~InterfaceVersion1() = default;
+
+		/// <summary>
+		/// Gets the current version of the interface.
+		/// </summary>
+		/// <returns></returns>
+		virtual Version GetVersion() = 0;
+
+
+		/// <summary>
+		/// Should not be using manually. Returns a TargetObject from an Argument, throwing an exception back to ARTH if invalid.
+		/// </summary>
+		/// <param name="arg"></param>
+		/// <returns></returns>
+		virtual Arthmetic::Target ArgAsObject(SWITCH_ARG* arg) = 0;
+
+
+		/// <summary>
+		/// Should not be using manually. Returns a Number from an Argument, throwing an exception back to ARTH if invalid.
+		/// </summary>
+		/// <param name="arg"></param>
+		/// <returns></returns>
+		virtual float ArgAsNumber(SWITCH_ARG* arg) = 0;
+
+
+		/// <summary>
+		/// Should not be using manually. Returns a String from an Argument, throwing an exception back to ARTH if invalid.
+		/// </summary>
+		/// <param name="arg"></param>
+		/// <returns></returns>
+		virtual const char* ArgAsString(SWITCH_ARG* arg) = 0;
+
+		/// <summary>
+		/// When a routine is flagged native this will be used to link a C++ function with it. Safe to call after PostPostLoad.
+		/// </summary>
+		/// <param name="routine_name">is the name of the routine you'd like this to define.</param>
+		/// <param name="function">is the function you'd like the routine to call when executed.</param>
+		/// <param name="constaints">are expected parameter constraints. Currently unimplemented, set to zero.</param>
+		virtual void LinkNativeFunction(std::string routine_name, NativeFormula function, Arthmetic::ParamConstraints constaints = 0) = 0;
+	};
+
+	using CurrentInterface = InterfaceVersion1;
+
+	inline CurrentInterface* Interface = nullptr;
+
+
+
+#ifdef ARITHMETIC_SOURCE 
+
+
+	CurrentInterface* InferfaceSingleton();
+
+	namespace detail
+	{
+		extern "C" __declspec(dllexport) void* ARTH_RequestInterfaceImpl(Version version)
+		{
+			CurrentInterface* result = InferfaceSingleton();
+
+			switch (version)
+			{
+			case Version::Version1:
+				return dynamic_cast<InterfaceVersion1*>(result);
+			default:
+				//Should show an error or something like that.
+				return nullptr;
+			}
+
+			return nullptr;
+		}
+	}
+
+#endif
+
+
+	/// <summary>
+	/// Accesses the Arithmetic Interface, safe to call PostLoad
+	/// </summary>
+	/// <param name="version"> to request.</param>
+	/// <returns>Returns void* of the interface, cast to the respective version.</returns>
+	inline void* RequestInterface(Version version)
+	{
+		typedef void* (__stdcall* RequestFunction)(Version);
+
+		static RequestFunction request_interface = nullptr;
+
+		HINSTANCE API = GetModuleHandle(ARITHMETIC_API_SOURCE_L);
+
+		if (API == nullptr) {
+			logger::critical("{} not found, API will remain non functional.", ARITHMETIC_API_SOURCE);
+			return nullptr;
+		}
+
+		request_interface = (RequestFunction)GetProcAddress(API, "ARTH_RequestInterfaceImpl");
+
+		if (request_interface) {
+			if (static unsigned int once = 0; once++)
+				logger::info("Successful module and request, ARTH");
+			
+		}
+		else {
+			logger::critical("Unsuccessful module and request, ARTH");
+			return nullptr;
+		}
+
+		auto intfc = (CurrentInterface*)request_interface(version);
+
+		return intfc;
+	}
+
+	/// <summary>
+	/// Accesses the ActorValueGenerator Interface, safe to call PostLoad
+	/// </summary>
+	/// <typeparam name="InterfaceClass">is the class derived from the interface to use.</typeparam>
+	/// <returns>Casts to and returns a specific version of the interface.</returns>
+	template <class InterfaceClass = CurrentInterface>
+	inline  InterfaceClass* RequestInterface()
+	{
+		static InterfaceClass* intfc = nullptr;
+
+		if (!intfc) {
+			intfc = reinterpret_cast<InterfaceClass*>(RequestInterface(InterfaceClass::VERSION));
+			
+			if constexpr (std::is_same_v<InterfaceClass, CurrentInterface>)
+				Interface = intfc;
+		}
+		
+		return intfc;
+	}
+
+}
+
+#ifndef ARITHMETIC_SOURCE
+
+namespace Arthmetic
+{
+	/// <summary>
+	/// The object that the routine was called on. A combinate of Context and a BaseObject.
+	/// </summary>
+	struct Target
+	{
+		
+		BaseObject* focus = nullptr;
+		TargetContext* context = nullptr;
+
+
+		Target() = default;
+		constexpr Target(BaseObject* _f) : focus(_f) {}
+		constexpr Target(BaseObject* _f, TargetContext* _c) : focus(_f) {}
+		Target& operator=(BaseObject* base) { focus = base; context = nullptr; return *this; }
+
+		operator BaseObject* () const { return focus; }
+
+		BaseObject* operator->() const
+		{
+			return focus;
+		}
+	};
+
+	/// <summary>
+	/// The arguments sent via routine. Cannot be created and are solely accessed via pointer.
+	/// </summary>
+	struct ResolvedArg
+	{
+		/// <summary>
+		/// Treats the Argument like a TargetObject. Throws back to ARTH if invalid.
+		/// </summary>
+		/// <returns></returns>
+		Target AsObject()
+		{
+			return ArithmeticAPI::RequestInterface()->ArgAsObject(this);
+		}
+
+		/// <summary>
+		/// Treats the Argument like a Number. Throws back to ARTH if invalid.
+		/// </summary>
+		/// <returns></returns>
+		float AsNumber()
+		{
+			return ArithmeticAPI::RequestInterface()->ArgAsNumber(this);
+		}
+
+		/// <summary>
+		/// Treats the Argument like a string. Throws back to ARTH if invalid.
+		/// </summary>
+		/// <returns></returns>
+		std::string AsString()
+		{
+			return ArithmeticAPI::RequestInterface()->ArgAsString(this);
+		}
+
+	private:
+
+
+		
+		ResolvedArg() = delete;
+		ResolvedArg(const ResolvedArg&) = delete;
+		ResolvedArg(ResolvedArg&&) = delete;
+
+		ResolvedArg& operator=(const ResolvedArg&) = delete;
+		ResolvedArg& operator=(ResolvedArg&&) = delete;
+		~ResolvedArg() = delete;
+	};
+}
+#endif
+
+#ifdef ARITHMETIC_API_SOURCE
+#undef ARITHMETIC_API_SOURCE
+#endif
